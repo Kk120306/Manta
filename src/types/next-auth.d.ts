08b626bb2ccc922f7fe5fb462e1968634d935932
Kai Kameyama <1203kk06@gmail.com>
2025-06-25 17:07:54 +0800

    creditential sign in

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+    interface User {
+        id: string;
+        username: string;
+        email: string;
+    }
+
+    interface Session {
+        user : User & {
+            username : string;
+        } 
+        token : {
+            username: string;
+        }
+    }
+}
