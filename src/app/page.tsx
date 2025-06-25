afcc083cfc95c97163c763987e17abf8121f48c4
Kai Kameyama <1203kk06@gmail.com>
2025-06-25 17:15:43 +0800

    light change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { buttonVariants } from "@/components/ui/button";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
-import User from "@/components/User";
+import User from '@/components/User';
 
 export default async function Home() {
     const session = await getServerSession(authOptions);
